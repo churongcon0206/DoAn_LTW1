af6df39eb2c085ef534ad4b94a4fdc84fe3ffd57
Huy Hoang <64836352+churongcon0206@users.noreply.github.com>
2021-01-02 08:08:11 +0700

    resitration update

--- DoAn_LTW1-H.Hoang/registration.php
+++ DoAn_LTW1-H.Hoang/registration.php
@@ -14,36 +14,7 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 
-<?php
 
-    require_once '../Function.php';
-
-    if(isset($_POST['username']))
-    {
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $RePassword = $_POST['re_pass'];
-        $Name = $_POST['Name'];
-        $address = $_POST['Address'];
-        $phone = $_POST['Phone'];
-        $birth = $_POST['Birth'];
-
-        var_dump($username,
-        $email ,
-        $password,
-        $RePassword,
-        $Name,
-        $address,
-        $phone, 
-        $birth );
-    }
-    else
-    {
-        
-    }
-
-?>
 <body>
 
     <div class="main">
@@ -56,51 +27,48 @@
                         <h2 class="form-title">Sign up</h2>
                         <form method="POST" class="register-form" id="register-form">
                             <div class="form-group">
-                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="username" id="name" placeholder="User Name" />
+                                <!-- <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label> -->
+                                <input type="text" name="Username" id="name" placeholder="User Name" />
                             </div>
                             <div class="form-group">
-                                <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email" id="email" placeholder="Your Email" />
+                                <!-- <label for="email"><i class="zmdi zmdi-email"></i></label> -->
+                               <input type="email"  name="email" id="email" placeholder="Your Email" />
                             </div>
-                            <div class="form-group">
-                                <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                             <div class="form-group">
+                                <!-- <label for="pass"><i class="zmdi zmdi-lock"></i></label> -->
                                 <input type="password" name="pass" id="pass" placeholder="Password" />
                             </div>
                             <div class="form-group">
-                                <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password" />
+                                <!-- <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label> -->
+                                <input type="password" name="re_password" id="re_password" placeholder="Repeat your password" />
                             </div>
-
-
-
-
                             <div class="form-group">
-                                <!-- <label for="email"><i class="zmdi zmdi-email"></i></label> -->
-                                <input type="text" name="Name" id="Name" placeholder="Your Name" />
+                                <!-- <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label> -->
+                                <input type="text" name="fullname" id="name" placeholder="Full Name" />
                             </div>
                             <div class="form-group">
                                 <!-- <label for="email"><i class="zmdi zmdi-email"></i></label> -->
-                                <input type="text" name="Address" id="Address" placeholder="Your Address" />
+                                <input type="date" name="birth" id="Birth" placeholder="Year Of Birth" />
+                            </div>
+                            
+                            <div class="form-group">
+                                <!-- <label for="email"><i class="zmdi zmdi-email"></i></label> -->
+                                <input type="text" name="address" id="Address" placeholder="Your Address" />
                             </div>
                             <div class="form-group">
                                 <!-- <label for="email"><i class="zmdi zmdi-email"></i></label> -->
-                                <input type="number" name="Phone" id="Phone" placeholder="Your Phone Number" />
+                                <input type="number" name="phone" id="Phone" placeholder="Your Phone Number" />
                             </div>
                             <div class="form-group">
-                                <!-- <label for="email"><i class="zmdi zmdi-email"></i></label> -->
-                                <input type="date" name="Birth" id="Birth" placeholder="Year Of Birth" />
-                            </div>
-
-
-
-
+                                <!-- <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label> -->
+                                <input type="file" name="image" id="image" placeholder="Avatar" require />
+                            </div>                      
                             <div class="form-group">
                                 <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
                                 <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
                             </div>
                             <div class="form-group form-button">
-                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register" />
+                                <input type="submit" name="register" id="register" class="form-submit" value="Register" />
                             </div>
                         </form>
                     </div>
@@ -120,3 +88,44 @@
 </body>
 
 </html>
+
+
+<!-- <?php
+
+
+    if(isset($_POST['register']))
+    {
+        $username = $_POST['Username'];
+        $password = $_POST['password'];
+        $repassword = $_POST['re_password'];
+        $fullname = $_POST['fullname'];
+        $birth = $_POST['birth'];
+        $address = $_POST['address'];
+        $phone = $_POST['phone'];
+        $image = $_FILES['image']['name'];
+        $image_tmp = $_FILES['image']['tmp_name']; 
+        $c_ip = getRealUserIp();
+
+        move_uploaded_file($image_tmpe,"Security/customer_images/$image");
+        $insert_customers = "insert into customers
+        (username, email,passsword ,re_password ,fullname , address ,phone ,birthday, image, ip) values ('$username','$email','$password','$repassword','$fullname','$address','$phone','$birth','$image','$c_ip')";
+        $run_customer = mysqli_query($con,$insert_customers);
+        $sel_cart = "select * from cart where ip_add='$c_ip'";
+        $run_cart = mysqli_query($con,$sel_cart);
+        $check_card = mysqli_num_rows($run,$run_cart);
+        if ($check_cart>0){
+
+            $_SESSION['email']=$email;
+            echo "<script>alert('You have been register Successfully ')</script>";
+            echo "<script> window.open(chekout.php','_seld')</script>";
+        }else
+        {
+         $_SESSION['email']=$email;
+            echo "<script>alert('You have been register Successfully ')</script>";
+            echo "<script> window.open(MainApp.php','_seld')</script>";
+
+        }
+    }
+    
+
+?> -->
